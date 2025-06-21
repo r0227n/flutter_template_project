cc552b65a6373958dfdfdd91665ea84a6354eec1
r0227n <developryo@gmail.com>
2025-06-21 20:14:17 +0900

    feat: GoRouter でルーティング

---
+++ apps/lib/router/routes.dart
@@ -0,0 +1,17 @@
+import 'package:apps/pages/hoge_page.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+part 'routes.g.dart';
+
+@TypedGoRoute<HomeRoute>(
+  path: '/',
+)
+class HomeRoute extends GoRouteData {
+  const HomeRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const MyHomePage(title: 'Flutter Demo Home Page');
+  }
+}
