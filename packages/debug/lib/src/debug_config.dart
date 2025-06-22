9a04e467e1bde7765cc33a75b3bdd737a8f4297a
r0227n <developryo@gmail.com>
2025-06-22 09:41:56 +0900

    revert: debugがステージングされていたため、削除

--- packages/debug/lib/src/debug_config.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-class DebugConfig {
-  static bool get isDebugMode => kDebugMode;
-  static bool get shouldEnableTalker => isDebugMode;
-  static bool get shouldEnableShakeDetector => isDebugMode;
-}
