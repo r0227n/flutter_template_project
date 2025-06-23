0938345bdffc34dc8d1c13de10d4ec0cf2ee29a4
r0227n <developryo@gmail.com>
2025-06-23 20:37:39 +0900

    test: CIのテスト用の変更

--- app/lib/pages/home/home_page.dart
+++ app/lib/pages/home/home_page.dart
@@ -33,7 +33,7 @@ class _HomePageState extends ConsumerState<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Home'),
         actions: [
           IconButton(
             onPressed: () async {
