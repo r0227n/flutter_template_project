cd88730f51570d3765e38d434a3fe9bbc40ce545
r0227n <developryo@gmail.com>
2025-09-13 13:34:17 +0900

    feat: add ui_constants with Spacing and Radius classes

---
+++ app/lib/core/constants/ui_constants.dart
@@ -0,0 +1,15 @@
+final class Spacing {
+  const Spacing._();
+
+  static const double xs = 4;
+  static const double s = 8;
+  static const double m = 16;
+  static const double l = 24;
+  static const double xl = 32;
+}
+
+final class Radius {
+  const Radius._();
+
+  static const double s = 4;
+}
