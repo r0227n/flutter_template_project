98e81e3a0e8020cdae8cf3f50ea38507ddb77a52
r0227n <developryo@gmail.com>
2025-09-21 21:36:02 +0900

    fix: update import path for app translations to use slang.g.dart

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:app/i18n/translations.g.dart' as app_translations;
+import 'package:app/core/i18n/slang.g.dart' as app_translations;
 import 'package:app/pages/home_page.dart';
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
